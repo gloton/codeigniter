ff37153a331b42ba759308a2ea23e8a01517d0a8
Gloton <gloton@gmail.com>
2013-12-09 13:53:01 -0500

    Se actualiza y se elimina registro de la tabla templates, el tutor va eliminando las lineas de codigo pero yo solo las comente en este commit y ahora las eliminare para que queden igual como el las deja

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -22,9 +22,34 @@ class Test extends CMS_Controller {
 		echo 'Index test...';
 	}
 	
-	public function metodo2() {
-		echo 'metodo2';
-		echo $this->admin_panel_uri();
+	public function metodo2( $var1 , $var2 ) 
+	{
+		$this->load->database();
+		/*
+		$this->db->insert( 
+						'templates' , 
+						array( 
+							'name'			=>	'default',
+							'description'	=>	'Template front-end',
+							'panel'			=>	'f',
+							'default'		=>	1
+						)
+		);
+		*/
+		/*
+		$this->db->where( 'id' , 2 )->update( 
+						'templates', 
+						array( 
+							'name'			=>	'default',
+							'description'	=>	'Template back-end modificado',
+							'panel'			=>	'b',
+							'default'		=>	1
+						)
+		);
+		*/
+		$this->db->where( array( 'id'=>2 ) )->delete( 'templates' );
+		echo '<pre>';
+		print_r($this->db->get( 'templates' )->result() );
 	}
 }
 
