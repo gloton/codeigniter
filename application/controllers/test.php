c606dbf0e3eab2ef5d07f025b18e4b5dd4815fa1
Gloton <gloton@gmail.com>
2013-12-09 07:55:07 -0500

    Video 1 - traer solu un registro (es una consulta un poco avanzada)

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -19,64 +19,18 @@ class Test extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		echo 'Index test...';
 	}
 	
-	public function metodo2() {
-		$this->load->database();
+	public function metodo2( $var1 , $var2 ) {
+		$this->load->model('test_model' , 't_model');
 		
-		/* 
+		//imprime todos los registros
+		//print_r($this->t_model->registro());
 		echo '<pre>';
-		>>>>Para obtener todas las filas
-		print_r($this->db->get('test')->result());
+		//imprime todos los registros
+		print_r($this->t_model->registro(array('nombre'),array('id' => 2) , 'object'));
 		echo '</pre>';
-		imprime
-		Array
-		(
-		    [0] => stdClass Object
-		        (
-		            [id] => 1
-		            [nombre] => nombre 1
-		            [direccion] => direccion 1
-		        )
-		
-		    [1] => stdClass Object
-		        (
-		            [id] => 2
-		            [nombre] => nombre2
-		            [direccion] => 
-		        )
-		
-		)
-
-		 */
-		/*
-		echo '<pre>';
-		>>>>Para obtener solo un campo
-		print_r($this->db->select('nombre')->get('test')->result());
-		echo '</pre>';
-		imprime
-		Array
-		(
-				[0] => stdClass Object
-				(
-						[nombre] => nombre 1
-				)
-		
-				[1] => stdClass Object
-				(
-						[nombre] => nombre2
-				)
-		
-		)
-		*/
-		echo '<pre>';
-		//para obtener varios campos los tengo que indicar en un array
-		print_r($this->db->select(array('id','direccion'))->get('test')->result());
-		echo '</pre>';
-		return;
-		$this->load->view('test/test');
-		$this->load->view('test/test');
 	}
 }
 
