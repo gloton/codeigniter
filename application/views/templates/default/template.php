583eee0f2dd5ecf016d9f62b33fea4251f5cb593
Gloton <gloton@gmail.com>
2013-12-15 14:28:21 -0500

    Se agrega un sidebar

--- application/views/templates/default/template.php
+++ application/views/templates/default/template.php
@@ -66,20 +66,20 @@
 
 
     <div class="container">
-
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>To see the difference between static and fixed top navbars, just scroll.</p>
-        <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
-        </p>
-      </div>
-<h1>Plantilla Default 1117</h1>
-<?php foreach ($_content as $_view) :?>
-<?php include $_view;?>
-<?php endforeach;?>
+		<div class="row">
+			<div class="col-md-8">
+			<?php foreach ($_content as $_view) :?>
+			<?php include $_view;?>
+			<?php endforeach;?>
+			</div>
+			<div class="col-md-4">
+				<ul class="nav nav-pills nav-stacked">
+				  <li class="active"><a href="#">Home</a></li>
+				  <li><a href="#">Profile</a></li>
+				  <li><a href="#">Messages</a></li>
+				</ul>
+			</div>
+		</div>
     </div> <!-- /container -->
 
 
